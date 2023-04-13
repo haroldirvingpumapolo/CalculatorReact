9c11a4d1bdf7744e2320f5a7ae538779618c163e
haroldirvingpumapolo <pumapoloharold123@gmail.com>
2023-04-13 12:47:00 -0500

    changed the functions with if and switch to short circuit, changed the syntax of the props and fixed the key

--- src/components/CalculatorButtons.jsx
+++ src/components/CalculatorButtons.jsx
@@ -2,85 +2,64 @@ import React from "react";
 import { arrButton } from "../arrButton";
 import Button from "./Button";
 
-function CalculatorButtons({ functions }) {
-  const {
-    resetResponse,
-    alterNegativeAndPositive,
-    percent,
-    decimal,
-    calculatingButton,
-    showPrevNumberButton,
-    showresult,
-  } = functions;
-
+function CalculatorButtons({
+  onResetResponse,
+  onAlterNegativeAndPositive,
+  onPercent,
+  onDecimal,
+  onCalculatingButton,
+  onShowPrevNumberButton,
+  onShowresult,
+}) {
   return (
     <>
       {arrButton.map((button, i) => {
-        switch (button.typebutton) {
-          case "resetResponse":
-            return (
-              <div key={i}>
-                <Button
-                  functionButton={resetResponse}
-                  valueButton={button.valueButton}
-                />
-              </div>
-            );
-          case "alterNegativeAndPositive":
-            return (
-              <div key={i}>
-                <Button
-                  functionButton={alterNegativeAndPositive}
-                  valueButton={button.valueButton}
-                />
-              </div>
-            );
-          case "percent":
-            return (
-              <div key={i}>
-                <Button
-                  functionButton={percent}
-                  valueButton={button.valueButton}
-                />
-              </div>
-            );
-          case "decimal":
-            return (
-              <div key={i}>
-                <Button
-                  functionButton={decimal}
-                  valueButton={button.valueButton}
-                />
-              </div>
-            );
-          case "calculatingButton":
-            return (
-              <div key={i}>
-                <Button
-                  functionButton={calculatingButton}
-                  valueButton={button.valueButton}
-                />
-              </div>
-            );
-          case "showresult":
-            return (
-              <div key={i}>
-                <Button
-                  functionButton={showresult}
-                  valueButton={button.valueButton}
-                />
-              </div>
-            );
-          default:
-            return (
-              <div key={i}>
-                <Button
-                  functionButton={showPrevNumberButton}
-                  valueButton={button.valueButton}
-                />
-              </div>
-            );
-        }
+        return (
+          <div key={i}>
+            {button.typebutton === "resetResponse" && (
+              <Button
+                functionButton={onResetResponse}
+                valueButton={button.valueButton}
+              />
+            )}
+            {button.typebutton === "alterNegativeAndPositive" && (
+              <Button
+                functionButton={onAlterNegativeAndPositive}
+                valueButton={button.valueButton}
+              />
+            )}
+            {button.typebutton === "percent" && (
+              <Button
+                functionButton={onPercent}
+                valueButton={button.valueButton}
+              />
+            )}
+            {button.typebutton === "decimal" && (
+              <Button
+                functionButton={onDecimal}
+                valueButton={button.valueButton}
+              />
+            )}
+            {button.typebutton === "calculatingButton" && (
+              <Button
+                functionButton={onCalculatingButton}
+                valueButton={button.valueButton}
+              />
+            )}
+            {button.typebutton === "showresult" && (
+              <Button
+                functionButton={onShowresult}
+                valueButton={button.valueButton}
+              />
+            )}
+            {button.typebutton === "numberButton" && (
+              <Button
+                functionButton={onShowPrevNumberButton}
+                valueButton={button.valueButton}
+              />
+            )}
+          </div>
+        );
       })}
     </>
   );

c7b177a30b7542875c486024b16bd75cfc307d90
haroldirvingpumapolo <pumapoloharold123@gmail.com>
2023-04-13 12:51:56 -0500

    delete unnecessary files

--- src/components/NumberButtons.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function NumberButtons({nameButton}) {
-  return <button className="w-full h-full border-2 border-gray-500 bg-slate-200">{nameButton}</button>;
-}
-
-export default NumberButtons;
